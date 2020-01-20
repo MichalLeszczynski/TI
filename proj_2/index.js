19d63f2179ff5e49d4d6732cdd30970d5e815767
MichalLeszczynski <mleszczynsk@gmail.com>
2020-01-20 23:16:39 +0100

    dirty version with everything working

--- proj_2/index.js
+++ proj_2/index.js
@@ -2,6 +2,8 @@ var api_url = "http://pascal.fis.agh.edu.pl:1036"
 var request;
 var objJSON;
 var id_mongo;
+const dbName = "ML_indexed_DB"
+
 function getRequestObject()      {
    if ( window.ActiveXObject)  {
       return ( new ActiveXObject("Microsoft.XMLHTTP")) ;
@@ -11,23 +13,48 @@ function getRequestObject()      {
       return (null) ;
    }
 }
+
+function clear() {
+   document.getElementById('result').innerHTML = ''; 
+   document.getElementById('data').innerHTML = '';  
+   document.getElementById('myCanvas').innerHTML = '';
+}
+
+function show_row_tab(row) {
+   var txt = "<tr>";
+   for ( var prop in row  ) {
+      if (prop != "_id"){
+         txt += "<td>" + row[prop]+" </td>";
+      }
+   }
+   txt += "</tr>";
+   return txt;
+}
+
+function show_row(row) {
+   var txt = "";
+   for ( var prop in row  ) {
+      if (prop != "_id"){
+         txt +=row[prop]+" ";
+      }
+   }
+   return txt;
+}
   
 // Lista rekordow w bazie
 function _list() {
-   document.getElementById('result').innerHTML = ''; 
-   document.getElementById('data').innerHTML = '';  
+   clear() 
    request = getRequestObject() ;
    request.onreadystatechange = function() {
       if (request.readyState == 4){
          objJSON = JSON.parse(request.response);
-         var txt = "";
+         var txt = "<table><thead>";
+         txt += "<th>Imię</th><th>Płeć</th><th>Poziom komfortu</th>";
+         txt += "</thead><tbody>";
          for ( var id in objJSON )  {
-             txt +=  id+": {" ;
-             for ( var prop in objJSON[id] ) {
-                 txt += prop+":"+objJSON[id][prop]+",";
-             }
-             txt +="}<br/>";
+            txt += show_row_tab(objJSON[id]);
          }
+         txt += "</tbody></table>"
          document.getElementById('result').innerHTML = txt;
       }
    }
@@ -37,6 +64,7 @@ function _list() {
   
 // Wstawianie rekordow do bazy
 function _ins_form() {
+   clear();
    var form1 = "<form name='add'><table>" ;
    form1    += "<tr><td>Imię</td><td><input type='text' name='fname' value='Imię' ></input></td></tr>";
 
@@ -45,7 +73,7 @@ function _ins_form() {
    form1    += "<option value='kobieta'>kobieta</option>" 
    form1    += "</select></td></tr>"
 
-   form1    += "<tr><td>Samopoczucie</td><td><select name='comfort'>";
+   form1    += "<tr><td>Samopoczucie</td><td><select type='number' name='comfort'>";
    form1    += "<option value='1' selected>1</option>" 
    form1    += "<option value='2'>2</option>" 
    form1    += "<option value='3'>3</option>" 
@@ -69,37 +97,61 @@ function _insert(form)  {
     user.fname = form.fname.value;
     user.sex = form.sex.value;
     user.comfort = form.comfort.value;
-    txt = JSON.stringify(user);
-    document.getElementById('result').innerHTML = ''; 
-    document.getElementById('data').innerHTML = '';  
-    request = getRequestObject() ;
-    request.onreadystatechange = function() {
-       if (request.readyState == 4 && request.status == 200 )    {
-          document.getElementById('result').innerHTML = request.response;
-       }
-    }
-    request.open("POST", api_url + "/add", true);
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.send(txt);
+    clear()  
+
+    if( navigator.onLine ) { 
+      result = JSON.stringify(user);
+      request = getRequestObject() ;
+      request.open("POST", api_url + "/add", true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(result);
+   } else {
+         const connection = window.indexedDB.open(dbName, 4);
+         connection.onupgradeneeded = function (event) {
+          const db = event.target.result;
+          const objectStore = db.createObjectStore('results', {autoIncrement: true});
+          console.log(objectStore);
+         };
+         connection.onsuccess = function (event) {
+          const db = event.target.result;
+          const transaction = db.transaction('results', 'readwrite');
+          const objectStore = transaction.objectStore('results');
+          const objectRequest = objectStore.put(user);
+          objectRequest.onerror = function (event) {
+              console.log("error");
+              console.log(event);
+          };
+
+          objectRequest.onsuccess = function (event) {
+              console.log("success");
+          };
+      }
+      
+  }
 }
+
   
 // Usuwanie rekordow z bazy danych
 function _del_list() {
-    document.getElementById('result').innerHTML = ''; 
-    document.getElementById('data').innerHTML = '';  
+   clear()  
+
     request = getRequestObject() ;
     request.onreadystatechange = function() {
        if (request.readyState == 4) { 
           objJSON = JSON.parse(request.response);
-          var txt = "<form  name='data'><select class='check' name='del' size='10'>";
+          
+
+          var txt = "<form  name='data'>"
+          txt += "<select class='check' name='del' size='10'>";
+
           for ( var id in objJSON ) {
-              txt +=  "<option value="+objJSON[id]["_id"]+" >"+id+": {" ;
-              for ( var prop in objJSON[id] ) {
-                  txt += prop+":"+objJSON[id][prop]+",";
-              }
-              txt +="}</option>";
+               txt +=  "<option value="+objJSON[id]["_id"]+" >" ;
+               txt += show_row(objJSON[id]);
+               txt +="</option>";
           }
-          txt += "</select><br/><input type='button' value='usun' onclick='_delete(this.form)'/></form>";
+          txt += "</select><br/>";
+          txt += "<input type='button' value='usun' onclick='_delete(this.form)'/></form>";
+
           document.getElementById('data').innerHTML = txt;
        }
     }
@@ -110,8 +162,8 @@ function _del_list() {
 function _delete(form) {
     var rec = form.del.selectedIndex;
     var id = document.getElementsByTagName('option')[rec].value;
-    document.getElementById('result').innerHTML = ''; 
-    document.getElementById('data').innerHTML = '';  
+    clear()  
+
     request = getRequestObject() ;
     request.onreadystatechange = function() {
        if (request.readyState == 4 )    {
@@ -125,21 +177,22 @@ function _delete(form) {
   
 // Poprawa rekordow w bazie danych
 function _upd_list() {
-       document.getElementById('result').innerHTML = ''; 
-       document.getElementById('data').innerHTML = '';  
+   clear()  
+
        request = getRequestObject() ;
        request.onreadystatechange = function() {
          if (request.readyState == 4)    { 
            objJSON = JSON.parse(request.response);
-           var txt = "<form  name='data'><select class='check' name='upd' size='10'>";
-           for ( var id in objJSON )  {
-              txt +=  "<option value="+objJSON[id]["_id"]+" >"+id+": {" ;
-              for ( var prop in objJSON[id] ) {
-                  txt += prop+":"+objJSON[id][prop]+",";
-              }
-              txt +="}</option>";
+           var txt = "<form  name='data'>"
+           txt += "<select class='check' name='upd' size='10'>";
+ 
+           for ( var id in objJSON ) {
+                txt +=  "<option value="+objJSON[id]["_id"]+" >" ;
+                txt += show_row(objJSON[id]);
+                txt +="</option>";
            }
-           txt += "</select><br/><input type='button' value='popraw' onclick='_upd_form(this.form)'/></form>";
+           txt += "</select><br/>";
+           txt += "<input type='button' value='popraw' onclick='_upd_form(this.form)'/></form>";
           document.getElementById('data').innerHTML = txt;
           }
        }
@@ -153,8 +206,8 @@ function _upd_list() {
 function _upd_form(form) {
     var rec = form.upd.selectedIndex;
     idmongo = document.getElementsByTagName('option')[rec].value;
-    document.getElementById('result').innerHTML = ''; 
-    document.getElementById('data').innerHTML = '';  
+    clear()  
+
   var form1 = "<form name='upd'><table>" ;
 
   form1    += "<tr><td>Imię</td><td><input type='text' name='fname' value='Imię' ></input></td></tr>";
@@ -164,7 +217,7 @@ function _upd_form(form) {
   form1    += "<option value='kobieta'>kobieta</option>" 
   form1    += "</select></td></tr>"
 
-  form1    += "<tr><td>Samopoczucie</td><td><select name='comfort'>";
+  form1    += "<tr><td>Samopoczucie</td><td><select type='number' name='comfort'>";
   form1    += "<option value='1' selected>1</option>" 
   form1    += "<option value='2'>2</option>" 
   form1    += "<option value='3'>3</option>" 
@@ -181,6 +234,8 @@ function _upd_form(form) {
   form1    += "</table></form>";
   document.getElementById('data').innerHTML = form1;
   document.getElementById('result').innerHTML = ''; 
+  document.getElementById('cnv').innerHTML = '';  
+
 }
   
 function _update(form, idmongo) {
@@ -192,8 +247,8 @@ function _update(form, idmongo) {
     txt = JSON.stringify(user);
     console.log(txt)
     console.log(idmongo)
-    document.getElementById('result').innerHTML = ''; 
-    document.getElementById('data').innerHTML = '';  
+    clear()  
+
     request = getRequestObject() ;
     request.onreadystatechange = function() {
          if (request.readyState == 4 && request.status == 200 )    {
@@ -204,3 +259,307 @@ function _update(form, idmongo) {
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     request.send(txt);
 }
+
+
+
+function stats() {
+
+   clear() 
+   request = getRequestObject() ;
+   request.onreadystatechange = function() {
+      if (request.readyState == 4){
+         objJSON = JSON.parse(request.response);
+         var k = 0;
+         var m = 0;
+         var sum_k = 0;
+         var sum_m = 0;
+         for ( var id in objJSON )  {
+            if (objJSON[id].sex == 'kobieta') {
+               k++;
+               sum_k += objJSON[id].comfort;
+            }
+            else {
+               m++;
+               sum_m += objJSON[id].comfort;
+            }
+
+         }
+         draw_stats(sum_k/k, sum_m/m);
+
+      }
+   }
+   request.open("GET", api_url +"/list", true);
+   request.send(null);
+
+}
+
+function draw_stats(srednia_k, srednia_m) {
+   document.getElementById('result').innerHTML = ''; 
+   document.getElementById('data').innerHTML = '';
+   document.getElementById('myCanvas').innerHTML = '<canvas id="cnv" width="300" height="200"></canvas><legend for="myCanvas"></legend>'
+
+   let myCanvas = document.getElementById('cnv');
+
+   myCanvas.width = 300;
+   myCanvas.height = 300;
+      
+   var ctx = myCanvas.getContext("2d");
+    
+   function drawLine(ctx, startX, startY, endX, endY,color){
+       ctx.save();
+       ctx.strokeStyle = color;
+       ctx.beginPath();
+       ctx.moveTo(startX,startY);
+       ctx.lineTo(endX,endY);
+       ctx.stroke();
+       ctx.restore();
+   }
+    
+   function drawBar(ctx, upperLeftCornerX, upperLeftCornerY, width, height,color){
+       ctx.save();
+       ctx.fillStyle=color;
+       ctx.fillRect(upperLeftCornerX,upperLeftCornerY,width,height);
+       ctx.restore();
+   }
+    
+   var kategorie = {
+      "Kobiety": srednia_k,
+      "Mężczyźni": srednia_m,
+   };
+    
+   var Barchart = function(options){
+       this.options = options;
+       this.canvas = options.canvas;
+       this.ctx = this.canvas.getContext("2d");
+       this.colors = options.colors;
+     
+       this.draw = function(){
+           var maxValue = 10;
+           
+           var canvasActualHeight = this.canvas.height - this.options.padding * 2;
+           var canvasActualWidth = this.canvas.width - this.options.padding * 2;
+    
+           //drawing the grid lines
+           var gridValue = 0;
+           while (gridValue <= maxValue){
+               var gridY = canvasActualHeight * (1 - gridValue/maxValue) + this.options.padding;
+               drawLine(
+                   this.ctx,
+                   0,
+                   gridY,
+                   this.canvas.width,
+                   gridY,
+                   this.options.gridColor
+               );
+                
+               //writing grid markers
+               this.ctx.save();
+               this.ctx.fillStyle = this.options.gridColor;
+               this.ctx.textBaseline="bottom"; 
+               this.ctx.font = "bold 10px Arial";
+               this.ctx.fillText(gridValue, 10,gridY - 2);
+               this.ctx.restore();
+    
+               gridValue+=this.options.gridScale;
+           }      
+     
+           //drawing the bars
+           var barIndex = 0;
+           var numberOfBars = Object.keys(this.options.data).length;
+           var barSize = (canvasActualWidth)/numberOfBars;
+    
+           for (categ in this.options.data){
+               var val = this.options.data[categ];
+               var barHeight = Math.round( canvasActualHeight * val/maxValue) ;
+               drawBar(
+                   this.ctx,
+                   this.options.padding + barIndex * barSize,
+                   this.canvas.height - barHeight - this.options.padding,
+                   barSize,
+                   barHeight,
+                   this.colors[barIndex%this.colors.length]
+               );
+    
+               barIndex++;
+           }
+    
+           //drawing series name
+           this.ctx.save();
+           this.ctx.textBaseline="bottom";
+           this.ctx.textAlign="center";
+           this.ctx.fillStyle = "#000000";
+           this.ctx.font = "bold 14px Arial";
+           this.ctx.fillText(this.options.seriesName, this.canvas.width/2,this.canvas.height);
+           this.ctx.restore();  
+            
+           //draw legend
+           barIndex = 0;
+           var legend = document.querySelector("legend[for='myCanvas']");
+           var ul = document.createElement("ul");
+           legend.append(ul);
+           for (categ in this.options.data){
+               var li = document.createElement("li");
+               li.style.listStyle = "none";
+               li.style.borderLeft = "20px solid "+this.colors[barIndex%this.colors.length];
+               li.style.padding = "5px";
+               li.textContent = categ;
+               ul.append(li);
+               barIndex++;
+           }
+       }
+   }
+    
+    
+   var myBarchart = new Barchart(
+       {
+           canvas:myCanvas,
+           seriesName:"Średnie samopoczucie",
+           padding:20,
+           gridScale:5,
+           gridColor:"#eeeeee",
+           data:kategorie,
+           colors:["#a55ca5","#67b6c7", "#bccd7a","#eb9743"]
+       }
+   );
+   myBarchart.draw();
+}
+
+
+function apply(response) {
+   objJSON = JSON.parse(response);
+   document.getElementById('for_logged').innerHTML = objJSON.for_logged;
+   document.getElementById('login').innerHTML = objJSON.login;  
+   document.getElementById('register').innerHTML = objJSON.register;
+   document.getElementById('logout').innerHTML = objJSON.logout;  
+   document.getElementById('result').innerHTML = objJSON.result;
+}
+
+function _login(form) {
+   clear() 
+   var user = {};
+   user.nick = form.nick.value;
+   user.password = form.password.value;
+   txt = JSON.stringify(user);
+
+   request = getRequestObject() ;
+   request.onreadystatechange = function() {
+      if (request.readyState == 4){
+         _init()
+      }
+   }
+   request.open("POST", api_url +"/login", true);
+   request.withCredentials = true;
+   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   request.send(txt);
+}
+
+function _logout(form) {
+   clear() 
+   request = getRequestObject() ;
+   request.onreadystatechange = function() {
+      if (request.readyState == 4){
+         apply(request.response)
+      }
+   }
+   request.open("POST", api_url +"/logout", true);
+   request.withCredentials = true;
+   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   request.send(null);
+}
+
+
+function _register(form)  {
+   var user = {};
+   user.nick = form.nick.value;
+   user.password = form.password.value;
+   txt = JSON.stringify(user);
+   clear()  
+
+   request = getRequestObject() ;
+   request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200 )    {
+         document.getElementById('result').innerHTML = request.response;
+      }
+   }
+   request.open("POST", api_url + "/register", true);
+   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   request.send(txt);
+}
+
+function _login_form() {
+   clear();
+   var form1 = "<form name='add'><table>" ;
+   form1    += "<tr><td>nick</td><td><input type='text' name='nick' value='nick' ></input></td></tr>";
+   form1    += "<tr><td>password</td><td><input type='password' name='password' value='password' ></input></td></tr>";
+   form1    += "<tr><td></td><td><input type='button' value='wyslij' onclick='_login(this.form)' ></input></td></tr>";
+   form1    += "</table></form>";
+   document.getElementById('data').innerHTML = form1;
+   document.getElementById('result').innerHTML = ''; 
+}
+
+function _register_form() {
+   clear();
+   var form1 = "<form name='add'><table>" ;
+   form1    += "<tr><td>nick</td><td><input type='text' name='nick' value='nick' ></input></td></tr>";
+   form1    += "<tr><td>password</td><td><input type='password' name='password' value='password' ></input></td></tr>";
+   form1    += "<tr><td></td><td><input type='button' value='wyslij' onclick='_register(this.form)' ></input></td></tr>";
+   form1    += "</table></form>";
+   document.getElementById('data').innerHTML = form1;
+   document.getElementById('result').innerHTML = ''; 
+}
+
+
+
+window.addEventListener('online', (event) => {
+   const connection = window.indexedDB.open(dbName, 4);
+   connection.onupgradeneeded = function (event) {
+       event.target.transaction.abort();
+       console.log(event);
+   };
+   connection.onsuccess = function (event) {
+       const db = event.target.result;
+       const transaction = db.transaction(['results'], "readwrite");
+       const objectStore = transaction.objectStore('results');
+       const objectRequest = objectStore.getAll();
+       objectRequest.onerror = function (event) {
+           console.log("error");
+           console.log(event);
+       };
+
+       objectRequest.onsuccess = function (event) {
+           console.log("success");
+           objectRequest.result.forEach(element => {
+               data = JSON.stringify(element);
+               console.log(data);
+               request = getRequestObject();
+               request.onreadystatechange = function() {
+                   if (request.readyState == 4 && request.status == 200 ) {}
+               }
+               request.open("POST", api_url + "/add", true);
+               request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+               request.send(data);
+           });
+           objectStore.clear();
+       };
+   }
+});
+
+
+function _init() {
+   request = getRequestObject() ;
+   request.onreadystatechange = function() {
+      if (request.readyState == 4){
+         objJSON = JSON.parse(request.response);
+         document.getElementById('for_logged').innerHTML = objJSON.for_logged  
+         document.getElementById('login').innerHTML = objJSON.login;  
+         document.getElementById('register').innerHTML = objJSON.register;
+         document.getElementById('logout').innerHTML = objJSON.logout;  
+         document.getElementById('result').innerHTML = objJSON.result;
+      }
+   }
+   request.open("GET", api_url +"/init", true);
+   request.withCredentials = true;
+   request.send(null);
+}
+
+_init()
--- proj_2/server.js
+++ proj_2/server.js
@@ -3,17 +3,31 @@ const express = require('express');
 const bodyParser= require('body-parser')
 var cors = require('cors')
 const mongodb = require('mongodb')
+const router = express.Router();
+var session = require('express-session')
+
 const MongoClient = mongodb.MongoClient
 var db
 var db_name = 'people'
+var db_users = 'users'
 
-  
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use(cors())
-  
+app.use(cors({ credentials: true, origin: "http://pascal.fis.agh.edu.pl" }))
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  // key: 'user_sid',
+  secret: 'somerandonstuffs',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      maxAge: 6000000
+  }
+}));
+
+
 MongoClient.connect('mongodb://7leszczynski:pass7leszczynski@172.20.44.25/7leszczynski', function(err, database) {
   if (err) return console.log(err)
   db = database
@@ -21,28 +35,24 @@ MongoClient.connect('mongodb://7leszczynski:pass7leszczynski@172.20.44.25/7leszc
 })
   
 
+
 app.listen(port,function() {
    console.log('listening on .........')
 })
   
-app.get('/', function(req,res) {
-  res.send('Aplikacja CRUD - node.js')
-})
   
-app.get('/form', function(req,res) {
-  res.sendFile(__dirname + '/form.html')
-})
-  
-app.post('/add', function( req,res ) {
+router.post('/add', function( req,res ) {
    console.log(req.body)
    db.collection(db_name).save(req.body, function(err,result) {
       if (err) return console.log(err)
       console.log('Rekord dodany do bazy')
+      db.collection(db_name).find().forEach( function (x) { x.comfort = parseInt(x.comfort); db.collection(db_name).save(x); })
       res.redirect('/list')
+      
    })
 })
   
-app.get('/list', function(req, res) {
+router.get('/list', function(req, res) {
   var cursor = db.collection(db_name).find().toArray(function(err, results) {
      if (err) return console.log(err);
      res.setHeader('Content-type', 'application/json');
@@ -51,19 +61,92 @@ app.get('/list', function(req, res) {
   })
 });
 
-app.put('/update/:id', function( req,res ) {
+router.put('/update/:id', function( req,res ) {
     var query = { _id: new mongodb.ObjectID(req.params.id)};
     var new_values = { $set: req.body };
     var cursor = db.collection(db_name).updateOne(query, new_values, function(err, obj) {
     if (err) throw err;
+    db.collection(db_name).find().forEach( function (x) { x.comfort = parseInt(x.comfort); db.collection(db_name).save(x); })
     console.log("1 document updated");
       });
   });
 
- app.delete('/delete/:id', function(req, res) {
+ router.delete('/delete/:id', function(req, res) {
     var query = { _id: new mongodb.ObjectID(req.params.id)};
     var cursor = db.collection(db_name).deleteMany(query, function(err, obj) {
     if (err) throw err;
     console.log('deleting id:' + req.params.id);
       });
   });
+
+  router.post('/register', function( req,res ) {
+    console.log(req.body)
+    db.collection(db_users).save(req.body, function(err,result) {
+       if (err) return console.log(err)
+       console.log('Rekord dodany do bazy')
+       res.send('Zarejestrowano')       
+    })
+ })
+
+  router.post('/login', function(req, res) {
+    var cursor = db.collection(db_users).find(req.body).toArray(function(err, results) {
+       if (err) return console.log(err);
+       if (results.length) {
+        req.session.user = req.body.nick;
+        console.log("Zalogowano", req.body);
+        res.send("Zalogowano")
+       }
+    })
+  });
+
+router.post('/logout', function(req, res) {
+
+    req.session.destroy((err) => {
+      if(err) {
+          return console.log(err);
+      }
+      var for_logged = '' 
+      var result =  "Wylogowano"
+      var logout = ''
+      var login = '<input type="button" value="Logowanie" onclick="_login_form()"/>'
+      var register = '<input type="button" value="Rejestracja" onclick="_register_form()"/>'
+
+      res.send({for_logged: for_logged, result: result, logout: logout, login: login, register: register});
+
+    });
+
+});
+
+router.get('/init', function(req, res) {
+  var for_logged = ''
+  var result = ''
+  var logout = ''
+  var login = ''
+  var register = ''
+  for(var o in req.session) {
+    console.log(o);
+  }
+
+  console.log("sess", req.session.user);
+
+  if (req.session.user) {
+    console.log("i am inside");
+    for_logged = '<input type="button" value="Pobranie danych z bazy" onclick="_list()"/> \
+    <input type="button" value="Dodanie rekordu do bazy" onclick="_ins_form()"/> \
+    <input type="button" value="Usuniecie rekordu z bazy" onclick="_del_list()"/> \
+    <input type="button" value="Poprawa rekordu w bazie" onclick="_upd_list()"/> \
+    <input type="button" value="Statystyki" onclick="stats()"/>'; 
+    result =  ''
+    logout = ' <input type="button" value="Wyloguj" onclick="_logout()"/>'
+   }
+   else {
+    for_logged = ''
+    result = ''
+    logout = ''
+    var login = '<input type="button" value="Logowanie" onclick="_login_form()"/>'
+    var register = '<input type="button" value="Rejestracja" onclick="_register_form()"/>'
+   }
+   res.send({for_logged: for_logged, result: result, logout: logout, login: login, register: register});
+});
+
+app.use('/', router);
